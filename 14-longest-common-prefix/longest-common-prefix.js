4f32a809d370ceedcac291c0db9d007cddb01bed
Muhammed Riswan P <muhammedriswanp7@gmail.com>
2025-09-10 21:42:38 +0530

    Time: 2 ms (42.80%) | Memory: 53.8 MB (75.98%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.js
+++ 14-longest-common-prefix/longest-common-prefix.js
@@ -2,16 +2,16 @@
  * @param {string[]} strs
  * @return {string}
  */
-var longestCommonPrefix = function(strs) {
-    string=""
-    for(i=0;i<strs[0].length;i++){
+var longestCommonPrefix = function (strs) {
+    string = ""
+    for (i = 0; i < strs[0].length; i++) {
         let char = strs[0][i]
-        for(j=1;j<strs.length;j++){
-            if(i>=strs[j].length || strs[j][i]!==char){
+        for (j = 1; j < strs.length; j++) {
+            if (strs[j][i] !== char) {
                 return string
             }
         }
-        string+=char
+        string += char
 
     }
     return string

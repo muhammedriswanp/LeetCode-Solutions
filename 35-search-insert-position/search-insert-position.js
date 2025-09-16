d43d23778a3c080d1869dc23050506948cbcdc9b
Muhammed Riswan P <muhammedriswanp7@gmail.com>
2025-09-16 21:38:07 +0530

    Time: 0 ms (100.00%) | Memory: 54.5 MB (24.98%) - LeetSync

---
+++ 35-search-insert-position/search-insert-position.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function(nums, target) {
+    if(nums.includes(target)){
+        return nums.indexOf(target)
+    }
+
+    for(i=0;i<=nums.length;i++){
+        if(nums[i]>target){
+            return i
+        }
+    }
+    return nums.length
+};

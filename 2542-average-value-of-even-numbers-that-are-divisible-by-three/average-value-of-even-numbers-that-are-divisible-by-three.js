c33ffa950f49092da2b7d9da5dc1868bab509d81
Muhammed Riswan P <muhammedriswanp7@gmail.com>
2025-09-23 20:37:58 +0530

    Time: 0 ms (100.00%) | Memory: 55.8 MB (41.52%) - LeetSync

--- 2542-average-value-of-even-numbers-that-are-divisible-by-three/average-value-of-even-numbers-that-are-divisible-by-three.js
+++ 2542-average-value-of-even-numbers-that-are-divisible-by-three/average-value-of-even-numbers-that-are-divisible-by-three.js
@@ -7,7 +7,7 @@ var averageValue = function(nums) {
    count = 0
     for (i=0;i<nums.length;i++){
 
-        if (nums[i]%3===0&& nums[i]%2==0){
+        if (nums[i]%6===0){
             
             number+=nums[i]
             count++

aa57d18e533b41fd4eeff4183d70ec5d3232d9b3
Muhammed Riswan P <muhammedriswanp7@gmail.com>
2025-10-04 20:25:52 +0530

    Time: 189 ms (99.30%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1390-average-selling-price/average-selling-price.sql
@@ -0,0 +1,5 @@
+select p.product_id,coalesce( round(sum(p.price * u.units )::decimal/sum(u.units ) ,2),0) as average_price 
+from Prices as p
+left join UnitsSold  as u
+on p.product_id = u.product_id AND u.purchase_date BETWEEN p.start_date AND p.end_date
+group by p.product_id;

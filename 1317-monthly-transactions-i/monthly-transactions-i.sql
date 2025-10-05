fca9dad9243378c3962ed1a3415c19559a0a2e58
Muhammed Riswan P <muhammedriswanp7@gmail.com>
2025-10-05 23:20:14 +0530

    Time: 406 ms (86.21%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1317-monthly-transactions-i/monthly-transactions-i.sql
@@ -0,0 +1,8 @@
+select to_char(trans_date , 'yyyy-mm') as month,
+  country , 
+  count (* ) as trans_count ,
+  count(case when state = 'approved'   then 1  end)as approved_count ,
+  sum(amount ) as trans_total_amount ,
+  sum (case when state='approved' then amount else 0 end  ) as approved_total_amount 
+from Transactions 
+group by to_char(trans_date , 'yyyy-mm') , country   ;

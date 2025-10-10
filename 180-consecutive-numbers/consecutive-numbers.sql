b77f6d1ffd3c707adb2c21a0a787c37e55dab879
Muhammed Riswan P <muhammedriswanp7@gmail.com>
2025-10-10 22:04:55 +0530

    Time: 283 ms (42.51%) | Memory: 0B (100.00%) - LeetSync

--- 180-consecutive-numbers/consecutive-numbers.sql
+++ 180-consecutive-numbers/consecutive-numbers.sql
@@ -1,3 +1,12 @@
-select distinct num as ConsecutiveNums  from (select num, lag(num) over (order by id ) as l, lead(num) over (order by id ) as le
-from Logs ) as tab
-where le=num and l=num;
+-- select distinct num as ConsecutiveNums  from (select num, lag(num) over (order by id ) as l, lead(num) over (order by id ) as le
+-- from Logs ) as tab
+-- where le=num and l=num;
+
+with cte as 
+    (select num, 
+    lag(num) over (order by id ) as l,
+    lead(num) over (order by id ) as le
+    from Logs)
+     select distinct num as ConsecutiveNums  
+     from cte
+      where le=num and l=num;

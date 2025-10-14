d572c4a36e785bedea4803e613a6d54d6e48b0f2
Muhammed Riswan P <muhammedriswanp7@gmail.com>
2025-10-14 15:22:37 +0530

    Time: 526 ms (99.93%) | Memory: 0B (100.00%) - LeetSync

--- 2087-confirmation-rate/confirmation-rate.sql
+++ 2087-confirmation-rate/confirmation-rate.sql
@@ -1,11 +1,8 @@
-
-    select s.user_id ,
-    coalesce (round(count(case when action = 'confirmed' then 1 end) ::decimal / 
-    nullif (count(action),0),2), 0.00)  as confirmation_rate 
-    from  Signups as s
-    left join  Confirmations as c
-    on c.user_id  = s.user_id 
-
-    group by s.user_id 
-
- ;
+select s.user_id ,
+round(
+count(case when c.action = 'confirmed' then 1 end ) / count(*),2)
+as confirmation_rate 
+from Signups  as s
+left join Confirmations as c
+on s.user_id = c.user_id 
+group by s.user_id  ;

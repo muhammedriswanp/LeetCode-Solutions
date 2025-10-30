9ebee37ad4ea51624ef3eee2e1b53b422788e981
Muhammed Riswan P <muhammedriswanp7@gmail.com>
2025-10-30 20:20:13 +0530

    Time: 365 ms (34.13%) | Memory: 0B (100.00%) - LeetSync

--- 607-sales-person/sales-person.sql
+++ 607-sales-person/sales-person.sql
@@ -1,9 +1,8 @@
-select s.name 
-from SalesPerson as s
-where s.sales_id not in (
+with cte as (
     select o.sales_id 
-    from Orders as o 
+    from Orders as o
     left join Company as c
     on o.com_id  = c.com_id 
-    where c.name   = 'RED'
-);
+    where c.name ='RED'
+)
+select s.name  from SalesPerson as s where sales_id not in (select * from cte) ;

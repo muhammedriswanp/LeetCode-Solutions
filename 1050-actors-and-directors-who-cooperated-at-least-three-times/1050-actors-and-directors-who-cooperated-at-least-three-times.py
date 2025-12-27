e7212bcaa45584a2b50916f9d230505f6db94a9a
Muhammed Riswan P <muhammedriswanp7@gmail.com>
2025-12-27 10:53:11 +0530

    Time: 287 ms (51.52%), Space: 68 MB (20%) - LeetHub

--- 1050-actors-and-directors-who-cooperated-at-least-three-times/1050-actors-and-directors-who-cooperated-at-least-three-times.py
+++ 1050-actors-and-directors-who-cooperated-at-least-three-times/1050-actors-and-directors-who-cooperated-at-least-three-times.py
@@ -1,6 +1,5 @@
 import pandas as pd
 
 def actors_and_directors(actor_director: pd.DataFrame) -> pd.DataFrame:
-    conditon = actor_director.groupby(['actor_id','director_id'])['director_id'].count().reset_index(name = 'cnt')
-    
-    return conditon[['actor_id','director_id']][conditon['cnt']>=3]
+    return actor_director.groupby(['actor_id','director_id']).filter(
+        lambda x: len(x)>=3)[['actor_id', 'director_id']].drop_duplicates()

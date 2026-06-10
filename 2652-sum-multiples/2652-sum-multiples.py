93d853640b1e684fe4e1bfee67de9ecf217e749c
Muhammed Riswan P <muhammedriswanp7@gmail.com>
2026-06-10 09:59:44 +0530

    Time: 30 ms (24.66%), Space: 19.2 MB (85.44%) - LeetHub

--- 2652-sum-multiples/2652-sum-multiples.py
+++ 2652-sum-multiples/2652-sum-multiples.py
@@ -1,7 +1,4 @@
 class Solution:
     def sumOfMultiples(self, n: int) -> int:
-        total = 0
-        for i in range(1,n+1):
-            if i%3 == 0 or i%5 == 0 or i%7 == 0:
-                total += i
-        return total
+        
+        return sum(i for i in range(1,n+1) if i%3 == 0 or i%5 == 0 or i%7 == 0)
